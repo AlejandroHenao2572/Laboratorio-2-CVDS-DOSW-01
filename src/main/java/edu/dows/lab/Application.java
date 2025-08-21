00ee02fb8d16f51a8bc852c38dfb88cd6c724c0c
Alejandro Henao <david.patacon-h@mail.escuelaing.edu.co>
2025-08-21 11:07:49 -0500

    feature: Preparacion - Subir Estructura de Paquetes del Laboratorio 02

---
+++ src/main/java/edu/dows/lab/Application.java
@@ -0,0 +1,7 @@
+package edu.dows.lab;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
